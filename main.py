10c2b49eeb2c0216548cec063dff12d30dcd762a
Diego Pereira <diegoo.junior55@gmail.com>
2021-10-02 15:04:01 -0300

    criado main parar chamar as informações dos pacotes

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
-from os import system
-import sys
+from time import sleep
 
 def main():  
-
-     while True:
-
-          print('Consumir dados API')
-          print('Selecione uma opção no menu: ')
-          print('1 - Intraday series')
-          print('2 - Daily series')
-
-          opcao = int(input('Digite um opção => '))
-
-          if opcao == 1:
-               print('=' * 60)
-               print('Seus dados Intraday Series estão sendo gerados ....')
-               import intraday_series
-               print('=' * 60)
-               break
-               
-               
-          elif opcao == 2:
-             
-               print('daily_series')
-               print('=' * 60) 
-          
-          else:
-               print('Entrada invalida')
      
-          #main()
+     print('Consumir dados API')
+     print('Selecione uma opção no menu: ')
+     print('1 - Intraday series')
+     print('2 - Daily series')
+     print('3 - Daily adjusted series')
+
+     opcao = int(input('Digite um opção => '))
+
+     if opcao == 1:
+          print('=' * 60)
+          print('Seus dados Intraday Series estão sendo gerados ....')
+          sleep(5)
+          import intraday_series
+          print('=' * 60)    
+               
+     elif opcao == 2:
+          print('=' * 60)
+          print('Seus dados Daily Series estão sendo gerados ....')
+          sleep(5)
+          import daily_series
+          print('=' * 60)
+
+     elif opcao == 3:
+          print('=' * 60)
+          print('Seus dados Daily Adjusted Series estão sendo gerados ....')
+          sleep(5)
+          import daily_adjusted_series
+          print('=' * 60)  
+
+     else:
+          print('Digite uma opção valida')              
 
 if __name__ == '__main__':
      main()
